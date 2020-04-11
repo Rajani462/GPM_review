95a67f48fada4141d6acf84af9307fb3c104b493
Rajani462 <rkpradhan462@gmail.com>
2020-04-12 00:28:31 +0200

    data_base splitted, readme updated

--- code/01_data_preparation.R
+++ code/01_data_preparation.R
@@ -1,2 +1,20 @@
 # Data pre-processing.
 
+source('./source/libraries.R')
+
+
+studies <- readRDS('./data/studies.Rds')
+
+#Dividing the data-base into multiple small data-table
+
+study_area <- studies[, .(id, study_area, study_area_type, country, continent, lat_mean, lon_mean, area)]
+gpm_types <- studies[, .(id, gpm_type, gpm_algorithm, record_start, record_end, record_length)]
+study_scale <- studies[, .(id, temporal_scale, grid_scale, comparison_scale, comprison_method)]
+study_stats <- studies[, .(id, timeseries_eval, categ_eval)]
+study_perfm <- studies[, .(best_perform, worst_perform, limitations, reference, year, journal)]
+
+#Example
+study_stats_split <- separate(study_stat, col = "timeseries_eval", into = c("v1", "v2", "v3", "v4", "v5", "v6", "v7" ))
+
+
+
--- source/libraries.R
+++ source/libraries.R
@@ -1,4 +1,5 @@
 #Libraries commonly used in code
 
 library(data.table)
+library(tidyr)
 library(ggplot2)
