93fc1bb21e9a951c2099e41a13cec3dfa2377aae
Rajani462 <rkpradhan462@gmail.com>
2020-04-16 11:04:03 +0200

    Updated function

--- source/functions.R
+++ source/functions.R
@@ -1,1 +1,12 @@
-##
+###spliting and tidy function
+
+split_tidy <- function(x) {
+names <- c("v1", "v2", "v3", "v4", "v5", "v6", "v7")
+value_name <- names(x[, 2])
+study_split <- separate(x, 2, sep = ',', into = names)
+study_tidy <- melt(study_split, id.vars = 'id', value.name = value_name) 
+study_tidy <- study_tidy[complete.cases(study_tidy)]
+study_tidy <- study_tidy[, variable := NULL]
+return(study_tidy)
+}
+
