02780a8b7109a8c62caa72db995056c8ac2aaf9e
Rajani462 <rkpradhan462@gmail.com>
2020-06-26 17:53:31 +0200

    data preparation for plot

--- source/plots.R
+++ source/plots.R
@@ -4,8 +4,12 @@ source('./source/palettes.R')
 ###############################
 #data preparation for plots
 
-study_plot <- studies[, .(id, study_area, study_area_type, country, continent, 
-                          lat_mean, lon_mean, area, variable, surface, record_length, year)]
+study_plot <- studies[, .(id, study_area, study_area_type,
+                          country, continent, 
+                          lat_mean, lon_mean, area,
+                          variable, surface, 
+                          downscale, record_length,
+                          year, journal)]
 
 
 
@@ -13,11 +17,9 @@ imerg_combi <- study_plot[alg_vers, on = 'id']
 imerg_combi <- imerg_combi[run_tpe, on = 'id']
 imerg_combi <- imerg_combi[study_gridscale, on = 'id']
 imerg_combi <- imerg_combi[study_tempscale, on = 'id']
+imerg_combi <- imerg_combi[study_compmthod, on  = 'id']
 
-
-Paper_num<- study_plot[,.(Papers_count = sum(year)),by=.(journal)]
-
-###########
+###########Spatial distribution of publication
 
 world <- ne_countries(scale = "medium", returnclass = "sf")
 class(world)
@@ -35,61 +37,83 @@ global_dist <- ggplot(data = world) +
 
 global_dist + theme_bw()
 
+######################################
+
+#data for continent and country wise bar plot
 
 
-#bar plot
+plot_data2 <- study_plot[, .('publication_count' = .N),
+                         by = continent]
+
+plot_data3 <- study_plot[, .('publication_count' = .N),
+                         by = .(country, continent)]
+
+#continent wise_reordered
+ggplot(plot_data2, aes(x = reorder(continent, publication_count),
+                       y = publication_count, fill = continent)) + 
+  geom_bar(stat = "identity") + 
+  labs(x = "Continent", y = "Number of puplications") + 
+  scale_fill_manual(values = c("#4D648D", "#337BAE",
+                               "#97B8C2",  "#739F3D",
+                               "#ACBD78",  
+                               "#F4CC70", "#EBB582")) + 
+  coord_flip() + 
+  theme_bw()
+
+#continent wise publications per year
 ggplot(study_plot) + 
   geom_bar(aes(x = factor(year), fill = continent)) + 
-  scale_fill_manual(values = c("#4D648D", "#337BAE", "#97B8C2",  "#739F3D", "#ACBD78",  
+  scale_fill_manual(values = c("#4D648D", "#337BAE",
+                               "#97B8C2",  "#739F3D",
+                               "#ACBD78",  
                                "#F4CC70", "#EBB582")) + 
   labs(x = "Year", y = "Number of papers") +
   theme_classic()
 
-
-####continent wise bar plot
-continent_wise <- ggplot(study_plot) +
-  aes(x = continent, fill = continent) +
-  geom_bar() + 
-  geom_text(stat = 'count', aes(label = stat(count), hjust = -0.1), size=3.1)
-
-continent_wise + coord_flip() + 
-  labs(x = "Continent", y = "Number of papers") + 
-  scale_fill_manual(values = c("#4D648D", "#337BAE", "#97B8C2",  "#739F3D", "#ACBD78",  
-                               "#F4CC70", "#EBB582")) + 
+#country wise_reoodered
+ggplot(plot_data3, aes(x = reorder(country, publication_count),
+                       y = publication_count, fill = country)) +
+  geom_bar(stat = "identity") + 
+  labs(x = "Country", y = "Number of puplications") + 
+  coord_flip() + 
   theme_bw()
 
-#bar plot of IMERG_alg and versions
-gpm_vers <- gpm_alg[gpm_vers, on = 'id']
+#################################################
 
 
-#plot
 
-ggplot(na.omit(gpm_alg), aes(variable_name)) + 
+#bar plot of IMERG_alg and version
+
+ggplot(na.omit(imerg_combi), aes(variable_name)) + 
   geom_bar() + 
   scale_x_discrete(drop = TRUE)
   
 #scatter plot
-ggplot(na.omit(imerg_combi), aes(x=lat_mean, y=lon_mean, col = alg_vers)) + 
-  geom_point()
+ggplot(na.omit(imerg_combi), aes(x=lat_mean, 
+                                 y=lon_mean, 
+                                 col = gpm_algorithm)) + 
+  geom_point() + 
+  theme_bw()
 
 ggplot(na.omit(imerg_combi), aes(x=lat_mean, y=lon_mean,
                                  size = record_length,
                                  col = gpm_algorithm)) + 
+  geom_point() + 
+  theme_classic()
+
+ggplot(imerg_combi, aes(conti, gpm_type)) + 
+  geom_line()
+
+ggplot(na.omit(imerg_combi), aes(record_length, year)) + 
   geom_point()
 
-ggplot(imerg_combi, aes(gpm_algorithm, gpm_type)) + 
-  geom_point()
-
-ggplot(na.omit(imerg_combi), aes(record_length, temporal_scale)) + 
+ggplot(na.omit(imerg_combi), aes(comparison_method, downscale)) + 
   geom_point()
 
 #box plot
-ggplot(na.omit(imerg_combi), aes(x=gpm_algorithm, y=record_length,
-                                 fill=comparison_scale)) +
+ggplot(na.omit(imerg_combi), aes(x=gpm_algorithm, 
+                                 y=record_length,
+                               fill= downscale)) +
   geom_boxplot(alpha=0.4) 
 #
-  stat_summary(aes(color=names),fun.y=mean, geom="point", shape=20, size=10) +
-  theme(legend.position="none") +
-  scale_fill_manual(values = palettes_bright$colset_cheer_brights) +
-  scale_color_manual(values = palettes_bright$colset_cheer_brights) +
-  theme_generic
+  
