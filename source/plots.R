ccd332aadca981014d285a2e3d674074e9146721
Rajani462 <rkpradhan462@gmail.com>
2020-07-06 01:32:39 +0200

    all the plots were changed to theme_bw class from theme_classic.

--- source/plots.R
+++ source/plots.R
@@ -65,7 +65,8 @@ p1 <- ggplot(plot_continents, aes(x = reorder(continent, prop),
                                "#F4CC70", "#EBB582")) + 
   coord_flip() + 
   theme_classic()
-ggsave("results/plots/paperfraction_per_continent.png", p1, dpi = 300, width = 170, height = 100, units = "mm")
+ggsave("results/plots/paperfraction_per_continent.png", p1,
+       dpi = 300, width = 170, height = 100, units = "mm")
 
 #continent wise papers per year
 p2 <- ggplot(study_plot) + 
@@ -76,7 +77,8 @@ p2 <- ggplot(study_plot) +
                                "#F4CC70", "#EBB582")) + 
   labs(x = "Year", y = "Number of papers") +
   theme_classic()
-ggsave("results/plots/papers_per_year_continents.png", p2, dpi = 300, width = 170, height = 100, units = "mm")
+ggsave("results/plots/papers_per_year_continents.png", p2,
+       dpi = 300, width = 170, height = 100, units = "mm")
 
 #country wise papers reoodered
 p3 <- ggplot(plot_country, aes(x = reorder(country, prop),
@@ -85,7 +87,8 @@ p3 <- ggplot(plot_country, aes(x = reorder(country, prop),
   labs(x = "Country", y = "Papers fraction") + 
   coord_flip() + 
   theme_classic()
-ggsave("results/plots/paperfraction_per_country.png", p3, dpi = 300, width = 150, height = 120, units = "mm")
+ggsave("results/plots/paperfraction_per_country.png", p3,
+       dpi = 300, width = 150, height = 120, units = "mm")
 
 #################################################
 
@@ -114,6 +117,9 @@ ggplot(na.omit(imerg_combi), aes(imerg_vers, year)) +
 ggplot(na.omit(imerg_combi), aes(comparison_method, downscale)) + 
   geom_point()
 
+ggplot(na.omit(imerg_combi), aes(imerg_vers, imerg_type)) + 
+  geom_point()
+
 #box plot
 ggplot(na.omit(imerg_combi), aes(x=imerg_vers, 
                                  y=record_length,
@@ -136,11 +142,19 @@ ggplot(na.omit(imerg_combi), aes(x=imerg_type,
 plot_recordlength <- study_plot[, .('paper_count' = .N),
                            by = .(record_length, continent)][, percent := round(paper_count / sum(paper_count) * 100, 2)]
 
-ggplot(plot_recordlength, aes(x = record_length)) +
-  geom_histogram() +
+ggplot(plot_recordlength, aes(x = record_length, y = paper_count)) +
+  geom_line() +
   facet_wrap(~ continent)
 
+ggplot(plot_recordlength, aes(x = factor(record_length), y = percent)) +
+  geom_bar(stat = "identity")
+
+
 #data_continent wise
+continent_period <- study_plot[, .(id, continent, record_length)]
+
+count_pap <- study_plot[, .(id, continent, record_length)]
+
 asia <- study_plot[continent == "Asia"]
 africa <- study_plot[continent == "Africa"]
 europe <- study_plot[continent == "Europe"]
@@ -153,51 +167,55 @@ plot_asia <- asia[, .('count' = .N),
                    by = .(record_length, continent)][, percent := round(count / sum(count) * 100, 2)]
 
 #bar plots continent wise
+trial <- study_plot[, .( 'count' = .N),
+              by = .(record_length, continent)]
 
-barplot(table(cut(asia$record_length, breaks = seq(0, 60, by = 6),
-                  labels =paste(seq(0, 50, by =6), seq(6, 60, by = 6), sep="-"))))
-barplot(table(cut(africa$record_length, breaks = seq(0, 55, by = 5))))
-barplot(table(cut(europe$record_length, breaks = seq(0, 55, by = 5))))
-barplot(table(cut(south_america$record_length, breaks = seq(0, 55, by = 5))))
-barplot(table(cut(north_america$record_length, breaks = seq(0, 55, by = 5))))
-barplot(table(cut(global$record_length, breaks = seq(0, 55, by = 5))))
+#divide the record_lengths into ranges (10 months each)
+record_length_asia <-as.data.table(table(cut(asia$record_length, breaks = seq(0, 60, by = 10))))
+record_length_asia[, continent_name := factor('Asia')]
 
+record_length_africa <-as.data.table(table(cut(africa$record_length, breaks = seq(0, 60, by = 10))))
+record_length_africa[, continent_name := factor('Africa')]
 
-record_length_asia <- table(cut(asia$record_length, breaks = seq(0, 60, by = 6)))
-record_length_asia <- as.data.frame(record_length_asia)
+record_length_europe <-as.data.table(table(cut(europe$record_length, breaks = seq(0, 60, by = 10))))
+record_length_europe[, continent_name := factor('Europe')]
 
+record_length_s_america <-as.data.table(table(cut(south_america$record_length, breaks = seq(0, 60, by = 10))))
+record_length_s_america[, continent_name := factor('South_America')]
 
-ggplot(record_length_asia, aes(x= factor(Var1), y = Freq)) + 
+record_length_n_america <-as.data.table(table(cut(north_america$record_length, breaks = seq(0, 60, by = 10))))
+record_length_n_america[, continent_name := factor('North_America')]
+
+#merge all the continents to a single data table
+merge_continents <- merge(record_length_asia, record_length_africa, all = TRUE)
+asia_afi_ero <- merge(merge_continents, record_length_europe, all = TRUE)
+asia_afi_ero_samer <- merge(asia_afi_ero, record_length_s_america, all = TRUE)
+asia_afi_ero_s_n_ame <- merge(asia_afi_ero_samer, record_length_n_america, all = TRUE)
+
+##and now the plot
+bar_plot <- ggplot(asia_afi_ero_s_n_ame) + 
+  geom_bar(aes(x= factor(V1),
+      y = N,
+      group = continent_name,
+      fill = continent_name,
+      color = continent_name),
+      stat = "identity",
+      position = position_dodge()) + 
+  labs(x = "Validation length (months)", y = "Papers") + 
+  scale_fill_manual(values = c("#4D648D", "#337BAE",
+                               "#97B8C2",  "#739F3D",
+                               "#ACBD78"))
+p4 <- bar_plot +  scale_x_discrete(labels = c("0-10", "10-20",
+                                              "20-30", "30-40", "40-50", "50-60")) + 
+  coord_flip()
+
+ggsave("results/plots/validation_lengths_continent.png",
+       p4, dpi = 300, width = 150, height = 120, units = "mm")
   
-  geom_bar(stat = "identity") +
-  
-  theme_classic()
+##############################
 
 ggplot(study_plot, aes(record_length, continent)) + 
   geom_boxplot()
+  
 
-
-ggplot(plot_asia, aes(x = record_length, y = percent)) + 
-  geom_line() + 
-  #geom_bar(stat = "identity") + 
-  theme_classic()
-
-
-ggplot(plot_recordlength, aes(x =  record_length,
-                                  y = percent, fill)) + 
-  geom_bar(stat = "identity") + 
-  theme_classic()
-
-hist(plot_recordlength[plot_recordlength$continent == "Asia"])
-
-plot_recordlength %>%
-  filter(continent == "Asia") %>%
-  ggplot() + 
-  geom_histogram(aes(x = record_length), fill = "grey", color = "black")
-
-ggplot(Asia, aes(x = record_length)) +
-  geom_bar()
-
-
-length <- Asia$record_length
 
