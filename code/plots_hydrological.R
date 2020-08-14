6702ec4724374d6f2d047d875224ade46cb68524
PRADHAN Rajani Kumar <pradhan@fzp.czu.cz>
2020-08-14 16:59:03 +0200

    hydrological plots without facets

--- code/plots_hydrological.R
+++ code/plots_hydrological.R
@@ -1,11 +1,17 @@
 ggplot(na.omit(hydro_metrics), aes(x=Data_type, 
-                                 y= NSE, 
-                                 shape = Model,
-                                 col = Data_type)) + 
-  labs(x = "IMERG_RUN", y = "NSE") +  
-  geom_jitter() + 
-  scale_color_discrete(guide = "none") + 
-  facet_wrap(~Location) + 
+                                   y= NSE)) + 
+  geom_point(aes(shape=Model, color = Location), size= 4) + 
+  scale_shape_manual(values=c(15, 13, 18, 20, 25)) + 
+  theme_generic
+
+
+NSE + geom_point(aes(colour = factor(Location)), size = 4) +
+  geom_point(colour = "grey90", size = 1.5)
+
+  
+   geom_point() + 
+  #scale_color_discrete(guide = "none") + 
+  #facet_wrap(~Location) + 
   theme_generic
 #geom_vline(aes(xintercept= Data_type), color="#990000", linetype="dashed")
 
