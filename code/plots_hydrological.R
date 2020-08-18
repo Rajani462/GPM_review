9effb2db8520185947d70a29e5efe9e002b26d6f
PRADHAN Rajani Kumar <pradhan@fzp.czu.cz>
2020-08-18 14:46:19 +0200

    Removed the IMERGFRUncal from plots

--- code/plots_hydrological.R
+++ code/plots_hydrological.R
@@ -1,9 +1,19 @@
+
+hydro_metrics <- subset(hydro_metrics, hydro_metrics$Data_type != "IMERGFRUnCal") #remove the IMERGFRUnCal from the database.
+
 ggplot(na.omit(hydro_metrics), aes(x=Data_type, 
                                    y= NSE)) + 
-  geom_point(aes(shape=Model, color = Location), size= 4) + 
+  geom_jitter(aes(shape=Model, color = Location), size= 4) + 
   scale_shape_manual(values=c(15, 13, 18, 20, 25)) + 
-  theme_generic
+  theme_small + 
+  geom_vline(aes(xintercept= Data_type), color="#990000", linetype="dashed")
 
+
+
+
+
+ggsave("results/plots/hydrological_NSE.png", width = 7.2,
+       height = 5.3, units = "in", dpi = 600)
 
 NSE + geom_point(aes(colour = factor(Location)), size = 4) +
   geom_point(colour = "grey90", size = 1.5)
@@ -13,7 +23,8 @@ NSE + geom_point(aes(colour = factor(Location)), size = 4) +
   #scale_color_discrete(guide = "none") + 
   #facet_wrap(~Location) + 
   theme_generic
-#geom_vline(aes(xintercept= Data_type), color="#990000", linetype="dashed")
+#geom_vline(aes(xintercept= c(1.5,2.5,3.5,4.5)), color="#990000", linetype="dashed")
+   #geom_vline(xintercept = c(1.5,2.5,3.5,4.5))
 
 ggplot(na.omit(hydro_metrics), aes(x=Data_type, 
                                    y= Bias, 
@@ -24,3 +35,6 @@ ggplot(na.omit(hydro_metrics), aes(x=Data_type,
   scale_color_discrete(guide = "none") + 
   facet_wrap(~Location) + 
   theme_small
+
+ggsave("results/plots/hydrological_RBIAS.png", width = 7.2,
+       height = 5.3, units = "in", dpi = 600)
