a92b59910d0c2a4d680f7c90d9d01996a5b10fa3
PRADHAN Rajani Kumar <pradhan@fzp.czu.cz>
2020-08-26 13:07:09 +0200

    arrage NSE_BIASplots into single panel

--- code/plots_hydrological.R
+++ code/plots_hydrological.R
@@ -1,11 +1,11 @@
 
 hydro_metrics <- subset(hydro_metrics, hydro_metrics$Data_type != "IMERGFRUnCal") #remove the IMERGFRUnCal from the database.
 
-ggplot(na.omit(hydro_metrics), aes(x=Data_type, 
-                                   y= NSE)) + 
+NSE <- ggplot(na.omit(hydro_metrics), aes(x=Data_type, y= NSE)) + 
+  labs(x = "IMERG_RUN", y = "NSE") + 
   geom_jitter(aes(shape=Model, color = Location), size= 4) + 
-  scale_shape_manual(values=c(15, 13, 18, 20, 25)) + 
-  theme_small + 
+  #scale_shape_manual(values=c(15, 13, 18, 20, 25)) + 
+  theme_small
   #geom_vline(aes(xintercept= Data_type), color="#990000", linetype="dashed")
 
 
@@ -16,15 +16,16 @@ ggsave("results/plots/hydrological_NSE.png", width = 7.2,
 
 
 
-ggplot(na.omit(hydro_metrics), aes(x=Data_type, 
-                                   y= Bias, 
-                                   shape = Model,
-                                   col = Data_type)) + 
+BIAS <- ggplot(na.omit(hydro_metrics), aes(x=Data_type, y= Bias)) + 
   labs(x = "IMERG_RUN", y = "Realative bias (%)") + 
-  geom_jitter() + 
+  geom_jitter(aes(shape=Model, color = Location), size= 4) + 
   scale_color_discrete(guide = "none") + 
-  facet_wrap(~Location) + 
-  theme_small
+  #facet_wrap(~Location) + 
+  theme_small + 
+  theme(legend.position = "none")
 
-ggsave("results/plots/hydrological_RBIAS.png", width = 7.2,
+
+#NSE_BIAS <- grid.arrange(NSE, BIAS, ncol = 2)
+NSE_BIAS <- ggarrange(NSE, BIAS, ncol = 2, common.legend = TRUE, legend = "bottom")
+ggsave("results/plots/hydrological_NSE_BIAS.png", NSE_BIAS, width = 7.2,
        height = 5.3, units = "in", dpi = 600)
