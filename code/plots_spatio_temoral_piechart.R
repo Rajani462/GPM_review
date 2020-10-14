0765711035681c9b7bdcee7fedc09ec4208fc591
Rajani <rkpradhan462@gmail.com>
2020-10-14 12:56:08 +0200

    Pie_chart_sapti_temporal_continent

---
+++ code/plots_spatio_temoral_piechart.R
@@ -0,0 +1,81 @@
+ref_spat_tempo <- refr_type[study_tempscale, on = 'id']
+ref_spat_tempo2 <- ref_spat_tempo[study_gridscale, on = 'id']
+ref_spat_tempo2<- ref_spat_tempo2[continent_type, on = 'id']
+
+ref_spat_tempo3 <- subset(ref_spat_tempo2, !is.na(temporal_scale))
+ref_spat_tempo4 <- subset(ref_spat_tempo3, !is.na( grid_scale))
+ref_spat_tempo5 <- subset(ref_spat_tempo4, !is.na(continent))
+#spat_tempo <- study_tempscale[study_gridscale, on = 'id']
+#spat_tempo_ref <- spat_tempo[refr_type, on = 'id']
+
+
+#subset_tempo <- imerg_combi[, .(continent, temporal_scale, grid_scale)]
+#ref_spat_tempo2$grid_scale <- factor(ref_spat_tempo2$grid_scale, 
+                                     #levels = c("0.1", "0.25"))
+ref_spat_tempo5$continent <- factor(ref_spat_tempo5$continent, 
+                                    levels = c("Africa", "Asia", "Europe", "North America",
+                                               "South America", "Global"))
+ref_spat_tempo5$temporal_scale <- factor(ref_spat_tempo5$temporal_scale, 
+                                         levels = c("0.5h", "1h", "3h", "6h", "12h", 
+                                                    "daily",  "monthly",  "seasonal", "annual"))
+ref_spat_tempo5$grid_scale <- factor(ref_spat_tempo5$grid_scale, 
+                                     levels = c("0.1", "0.25", "0.5", "1", "2", "2.5", "3"))
+subset_tempo <- ref_spat_tempo5[, .(count = .N), by = .(grid_scale, temporal_scale, continent)]
+
+
+#subset_tempo[, x2 := unclass(continent)]
+#subset_tempo[, y2 := unclass(temporal_scale)]
+
+df_plot <- as.data.frame(subset_tempo) %>% 
+  group_by(grid_scale, temporal_scale) %>%
+  #do(data_frame(component = LETTERS[1:3], value = runif(3))) %>% 
+  mutate(total = sum(count)) %>%
+  group_by(grid_scale, temporal_scale, total)
+
+
+df_plot
+#total_studies <- sum(df_plot$total)
+#df_plot <- mutate(df_plot, percent = (total/total_studies)*100)
+
+df_plot$grid_scale <- unclass(df_plot$grid_scale)
+df_plot$temporal_scale <- unclass(df_plot$temporal_scale)
+
+colset_bright2 <- c("#6a3d9a", "#375E97")
+
+df.grobs <-  df_plot%>% 
+  do(subplots = ggplot(., aes(1, count, fill = continent)) + 
+       geom_col(position = "fill", alpha = 0.75, colour = "white") + 
+       coord_polar(theta = "y") + 
+       scale_fill_manual(labels = c("Africa", "Asia", "Europe", "North America",
+       "South America", "Global"), values=mycol_continent6) + 
+       theme_void()+ guides(fill = F)) %>% 
+  mutate(subgrobs = list(annotation_custom(ggplotGrob(subplots),
+                                           x = grid_scale-10/18, y = temporal_scale-10/18, 
+                                           xmax = grid_scale+10/18, ymax = temporal_scale+10/18))) #size of the pie charts
+df.grobs  <- subset(df.grobs , !is.na(temporal_scale))
+df.grobs  <- subset(df.grobs, !is.na( grid_scale))
+df.grobs  <- subset(df.grobs, !is.na(continent))
+
+
+
+
+final_plot <- df.grobs %>%
+  {ggplot(data = ., aes(factor(grid_scale), factor(temporal_scale))) + 
+      #scale_x_continuous(expand=c(0,0),"Validation lenghth",breaks=c(1,2,3,4,5),
+      # labels=c("0-12","13-24","25-36","37-48","49-60"), limits=c(0.5,6)) + 
+      #scale_x_discrete("Continent", labels = c("1" = "Africa", "2" = "Asia", "3" = "Europe", 
+      scale_x_discrete("spatial_scale", labels = c("1" = "0.1", "2" = "0.25", "3"  = "0.5", 
+                                                   "4"= "1", "5" = "2", "6" = "2.5", "7" = "3")) + 
+      scale_y_discrete("Temporal scale", labels = c("1" = "0.5h", "2" = "1h", "3" = "3h",
+                                                    "4" = "6h", "5" = "12h", "6" = "daily",
+                                                    "7" = "monthly", "8" = "seasonal",
+                                                    "9" = "annual")) + 
+      .$subgrobs + 
+      geom_text(aes(label = round(total, 2)), size = 3) + 
+      geom_col(data = df_plot,
+               aes(0,0, fill = continent), 
+               colour = "white") + 
+    scale_fill_manual("Continent", labels = c("Africa", "Asia", "Europe", "North America",
+                                                  "South America", "Global"), values=mycol_continent6)}
+
+final_plot + theme_small + theme(axis.text.x = element_text(angle = 40, hjust = 0.8, vjust = 0.9))
---
+++ code/plots_spatio_temoral_piechart_2.R
@@ -0,0 +1,87 @@
+ref_spat_tempo <- refr_type[study_tempscale, on = 'id']
+ref_spat_tempo2 <- ref_spat_tempo[study_gridscale, on = 'id']
+ref_spat_tempo2<- ref_spat_tempo2[continent_type, on = 'id']
+
+ref_spat_tempo3 <- subset(ref_spat_tempo2, !is.na(temporal_scale))
+ref_spat_tempo4 <- subset(ref_spat_tempo3, !is.na( grid_scale))
+ref_spat_tempo5 <- subset(ref_spat_tempo4, !is.na(continent))
+#spat_tempo <- study_tempscale[study_gridscale, on = 'id']
+#spat_tempo_ref <- spat_tempo[refr_type, on = 'id']
+
+
+#subset_tempo <- imerg_combi[, .(continent, temporal_scale, grid_scale)]
+#ref_spat_tempo2$grid_scale <- factor(ref_spat_tempo2$grid_scale, 
+#levels = c("0.1", "0.25"))
+ref_spat_tempo5$continent <- factor(ref_spat_tempo5$continent, 
+                                    levels = c("Africa", "Asia", "Europe", "North America",
+                                               "South America", "Global"))
+ref_spat_tempo5$temporal_scale <- factor(ref_spat_tempo5$temporal_scale, 
+                                         levels = c("0.5h", "1h", "3h", "6h", "12h", 
+                                                    "daily",  "monthly",  "seasonal", "annual"))
+ref_spat_tempo5$grid_scale <- factor(ref_spat_tempo5$grid_scale, 
+                                     levels = c("0.1", "0.25", "0.5", "1", "2", "2.5", "3"))
+subset_tempo <- ref_spat_tempo5[, .(count = .N), by = .(continent, temporal_scale, grid_scale)]
+
+subset_tempo <- subset(ref_spat_tempo, !is.na(temporal_scale))
+subset_tempo <- subset(ref_spat_tempo, !is.na( grid_scale))
+subset_tempo <- subset(ref_spat_tempo, !is.na(continent))
+
+subset_tempo <- subset(subset_tempo, !is.na(grid_scale))
+#subset_tempo[, x2 := unclass(continent)]
+#subset_tempo[, y2 := unclass(temporal_scale)]
+
+df_plot <- as.data.frame(subset_tempo) %>% 
+  group_by(continent, temporal_scale) %>%
+  #do(data_frame(component = LETTERS[1:3], value = runif(3))) %>% 
+  mutate(total = sum(count)) %>%
+  group_by(continent, temporal_scale, total)
+
+
+df_plot
+#total_studies <- sum(df_plot$total)
+#df_plot <- mutate(df_plot, percent = (total/total_studies)*100)
+
+df_plot$continent <- unclass(df_plot$continent)
+df_plot$temporal_scale <- unclass(df_plot$temporal_scale)
+
+mycol_gridscale7 <- c( "#69bdd2", "#739F3D", "#1979a9", "#edb879", "#e07b39", 
+                       "#80391e", "#6a3d9a")
+
+
+df.grobs <-  df_plot%>% 
+  do(subplots = ggplot(., aes(1, count, fill = !is.na(grid_scale))) + 
+       geom_col(position = "fill", alpha = 0.75, colour = "white") + 
+       coord_polar(theta = "y") + 
+       scale_fill_manual(labels = c("0.1", "0.25", "0.5", "1", "2", "2.5", "3"), 
+                         values=mycol_gridscale7) + 
+       theme_void()+ guides(fill = F)) %>% 
+  mutate(subgrobs = list(annotation_custom(ggplotGrob(subplots),
+                                           x = continent-10/18, y = temporal_scale-10/18, 
+                                           xmax = continent+10/18, ymax = temporal_scale+10/18))) #size of the pie charts
+
+df.grobs  <- subset(df.grobs , !is.na(temporal_scale))
+df.grobs  <- subset(df.grobs, !is.na(grid_scale))
+df.grobs  <- subset(df.grobs, !is.na(continent))
+
+
+
+final_plot <- df.grobs %>%
+  {ggplot(data = ., aes(factor(continent), factor(temporal_scale))) + 
+      #scale_x_continuous(expand=c(0,0),"Validation lenghth",breaks=c(1,2,3,4,5),
+      # labels=c("0-12","13-24","25-36","37-48","49-60"), limits=c(0.5,6)) + 
+      #scale_x_discrete("Continent", labels = c("1" = "Africa", "2" = "Asia", "3" = "Europe", 
+      scale_x_discrete("Continents", labels = c("1" = "Africa", "2" = "Asia", "3"  = "Europe", 
+                                                   "4"= "North Americ", "5" = "South America", "6" = "Golbal")) + 
+      scale_y_discrete("Temporal scale", labels = c("1" = "0.5h", "2" = "1h", "3" = "3h",
+                                                    "4" = "6h", "5" = "12h", "6" = "daily",
+                                                    "7" = "monthly", "8" = "seasonal",
+                                                    "9" = "annual")) + 
+      .$subgrobs + 
+      geom_text(aes(label = round(total, 2)), size = 3) + 
+      geom_col(data = df_plot,
+               aes(0,0, fill = !is.na (grid_scale)), 
+               colour = "white") + 
+      scale_fill_manual("grid scale", labels = c("0.1", "0.25", "0.5", "1", "2", "2.5", "3"),
+                        values=mycol_gridscale7)}
+
+final_plot + theme_small + theme(axis.text.x = element_text(angle = 40, hjust = 0.8, vjust = 0.9))
