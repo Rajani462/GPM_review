78a1301cc58c0cea28ec691d5fd1e122b354ea63
Rajani <rkpradhan462@gmail.com>
2020-11-03 00:08:15 +0100

    minor changes

--- code/plots_spatio_temoral_piechart_2.R
+++ code/plots_spatio_temoral_piechart_2.R
@@ -1,6 +1,14 @@
-ref_spat_tempo <- refr_type[study_tempscale, on = 'id']
-ref_spat_tempo2 <- ref_spat_tempo[study_gridscale, on = 'id']
+#ref_spat_tempo <- refr_type[study_tempscale, on = 'id']
+ref_spat_tempo2 <- study_tempscale[study_gridscale, on = 'id']
 ref_spat_tempo5<- ref_spat_tempo2[continent_type, on = 'id']
+
+#write_xlsx(study_tempscale,  "study_tempscale.xlsx")
+#write_xlsx(ref_spat_tempo5,  "tempo_grid_conti.xlsx")
+
+
+
+
+trii <- ref_spat_tempo5[, .N, by = list(temporal_scale, grid_scale, continent)]
 
 
 
@@ -14,7 +22,7 @@ ref_spat_tempo5$continent <- factor(ref_spat_tempo5$continent,
 ref_spat_tempo5$temporal_scale <- factor(ref_spat_tempo5$temporal_scale, 
                                          levels = c("0.5h", "1h", "3h", "6h", "12h", 
                                                     "daily",  "monthly",  "seasonal", "annual"))
-#ref_spat_tempo5$grid_scale <- as.factor(ref_spat_tempo5$grid_scale, 
+ref_spat_tempo5$grid_scale <- as.factor(ref_spat_tempo5$grid_scale)
                                      #levels = c("0.1", "0.25", ">0.25"))
 
 
@@ -31,7 +39,6 @@ subset_tempo <- ref_spat_tempo5[, .(count = .N), by = .(continent, temporal_scal
 
 df_plot <- as.data.frame(subset_tempo) %>% 
   group_by(continent, temporal_scale) %>%
-  #do(data_frame(component = LETTERS[1:3], value = runif(3))) %>% 
   mutate(total = sum(count)) %>%
   group_by(continent, temporal_scale, total)
 
@@ -44,22 +51,24 @@ df_plot$continent <- unclass(df_plot$continent)
 df_plot$temporal_scale <- unclass(df_plot$temporal_scale)
 df_plot$grid_scale <- as.factor(df_plot$grid_scale)
 
+#write_xlsx(df_plot,  "df_plot3.xlsx")
+
 mycol_gridscale7 <- c( "#69bdd2", "#739F3D", "#e07b39")
 
 #df_plot2 <- df_plot[df_plot$grid_scale>0.25,`:=`("grid_scale" = factor(123))]
 
 df.grobs <-  df_plot%>% 
-  do(subplots = ggplot(., aes(1, count, fill = grid_scale)) + 
+  do(subplots = ggplot(., aes(1, count, fill = factor(grid_scale))) + 
        geom_col(position = "fill", alpha = 0.75, colour = "white") + 
        coord_polar(theta = "y") + 
-       scale_fill_manual(labels = c("0.1", "0.25", ">0.25"), 
-                         values=mycol_gridscale7) + 
+       #scale_fill_manual(labels = c("0.1", "0.25", ">0.25"), 
+                         #values=mycol_gridscale7) + 
        theme_void()+ guides(fill = F)) %>% 
   mutate(subgrobs = list(annotation_custom(ggplotGrob(subplots),
                                            x = continent-12/20, y = temporal_scale-12/20, 
                                            xmax = continent+12/20, ymax = temporal_scale+12/20))) #size of the pie charts
 
-
+#write_xlsx(df.grobs,  "df.grobs.xlsx")
 
 final_plot <- df.grobs %>%
   {ggplot(data = ., aes(factor(continent), factor(temporal_scale))) + 
@@ -77,9 +86,9 @@ final_plot <- df.grobs %>%
       geom_text(aes(label = round(total, 2)), size = 3) + 
       geom_col(data = df_plot,
                aes(0,0, fill = grid_scale), 
-               colour = "white") + 
-      scale_fill_manual("Spatial scale", labels = c("0.1", "0.25", ">0.25"),
-                        values=mycol_gridscale7)}
+               colour = "white")} 
+      #scale_fill_manual("Spatial scale", labels = c("0.1", "0.25", ">0.25"),
+                        #values=mycol_gridscale7)}
 
 final_plot + theme_small + 
   theme(axis.text.x = element_text(angle = 40, hjust = 0.8, vjust = 0.9))
@@ -87,3 +96,4 @@ final_plot + theme_small +
 
 ggsave("results/spatio_temporal_continents2.png",
        width = 7.2, height = 5.3, units = "in", dpi = 600)
+
